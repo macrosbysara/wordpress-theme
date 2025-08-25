e3efbed124678fd0d5abd75d8f6d842d5d546a83
KJ Roelke <kj.roelke@gmail.com>
2025-08-24 21:33:37 -0500

    phpcbf and composer fix

--- inc/theme/class-theme-init.php
+++ inc/theme/class-theme-init.php
@@ -70,11 +70,11 @@ class Theme_Init {
 	 * @param ?array $config the configuration array. Null if user is logged-in.
 	 * @return ?array The new config file, or null
 	 */
-	public function handle_speculative_loading(  $config ) {
+	public function handle_speculative_loading( $config ) {
 		if ( is_array( $config ) ) {
-            $config['mode']      = 'auto';
-            $config['eagerness'] = 'moderate';
-        }
-        return $config;
+			$config['mode']      = 'auto';
+			$config['eagerness'] = 'moderate';
+		}
+		return $config;
 	}
 }
