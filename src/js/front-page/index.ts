5e1999c8d9fb68249c7988100a89a769afbd4590
KJ Roelke <kj.roelke@gmail.com>
2025-11-01 19:37:01 -0500

    init theme migration

---
+++ src/js/front-page/index.ts
@@ -0,0 +1,12 @@
+document.addEventListener('wpcf7submit', function(event) {
+  const form = event.target;
+  const location = form.closest('.x-form-integration')?.getAttribute('data-form-location') || 'unknown';
+  const formId = form.closest('[data-wpcf7-id]')?.getAttribute('data-wpcf7-id') || null;
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'cf7_form_submit',
+    'formLocation': `home-${location}`,
+    'formId': formId
+  });
+}, false);
