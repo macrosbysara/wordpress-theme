ff9bfb48036f7290d3c420e935447fdb663c3e62
KJ Roelke <kj.roelke@gmail.com>
2025-11-04 22:14:28 -0600

    add styles and patterns

---
+++ patterns/pricing-columns.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Title: Pricing Columns
+ * Slug: macrosbysara/pricing-columns
+ * Categories: columns
+ * Description: A three-column pricing table layout.
+ *
+ * @package MacrosBySara
+ */
+
+?>
+<!-- wp:columns {"className":"align-items-stretch","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|xl","left":"var:preset|spacing|base"}}}} -->
+<div class="wp-block-columns align-items-stretch" style="flex-direction:row"><!-- wp:column {"className":"d-flex flex-column","style":{"spacing":{"blockGap":"var:preset|spacing|base"}}} -->
+<div class="wp-block-column d-flex flex-column"><!-- wp:group {"metadata":{"name":"Pricing Card"},"className":"flex-shrink-0 flex-grow-1","style":{"border":{"radius":"20px","width":"2px"},"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"},"blockGap":"var:preset|spacing|base"}},"borderColor":"drop-shadow","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group flex-shrink-0 flex-grow-1 has-border-color has-drop-shadow-border-color" style="border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--lg);flex-direction:column"><!-- wp:group {"templateLock":"insert","lock":{"move":false,"remove":true},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-light"}}},"typography":{"lineHeight":"1"}},"textColor":"primary-light","fontSize":"xxxl"} -->
+<p class="has-primary-light-color has-text-color has-link-color has-xxxl-font-size" style="line-height:1">$249</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-light"}}}},"textColor":"primary-light","fontSize":"md"} -->
+<p class="has-primary-light-color has-text-color has-link-color has-md-font-size">/mo</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"level":3,"lock":{"move":false,"remove":true}} -->
+<h3 class="wp-block-heading">Gold</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This is for you if you are:&nbsp;<strong><em>new</em>&nbsp;to tracking macros</strong>&nbsp;or&nbsp;<strong>want extra support with access to ask questions anytime</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"lock":{"move":true,"remove":true},"className":"mt-auto","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-buttons mt-auto"><!-- wp:button {"style":{"border":{"radius":"10px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://docs.google.com/forms/d/e/1FAIpQLSf74ACtDJG9BNaxxfwZcXZDLP45ESWHjMOCIbSukA_eGC5qHQ/viewform" style="border-radius:10px" target="_blank" rel="noreferrer noopener">Apply</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|base","left":"var:preset|spacing|base"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--base);padding-left:var(--wp--preset--spacing--base)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p>Everything in the 1:1 coaching plan plus...</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p>Weekly phone or Zoom calls</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p>Anytime messaging for questions</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"className":"d-flex flex-column","style":{"spacing":{"blockGap":"var:preset|spacing|base"}}} -->
+<div class="wp-block-column d-flex flex-column"><!-- wp:group {"metadata":{"name":"Pricing Card"},"className":"flex-shrink-0 flex-grow-1","style":{"border":{"radius":"20px","width":"2px"},"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"},"blockGap":"var:preset|spacing|base"}},"borderColor":"drop-shadow","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group flex-shrink-0 flex-grow-1 has-border-color has-drop-shadow-border-color" style="border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--lg);flex-direction:column"><!-- wp:group {"templateLock":"insert","lock":{"move":false,"remove":true},"style":{"spacing":{"blockGap":"0"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group has-secondary-color has-text-color has-link-color" style="flex-direction:row"><!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"typography":{"lineHeight":"1"}},"fontSize":"xxxl"} -->
+<p class="has-xxxl-font-size" style="line-height:1">$219</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"lock":{"move":true,"remove":true},"fontSize":"md"} -->
+<p class="has-md-font-size">/mo</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"level":3,"lock":{"move":false,"remove":true}} -->
+<h3 class="wp-block-heading">Silver</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This is for you if you are:&nbsp;<strong>familiar</strong>&nbsp;with tracking macros but like the option to&nbsp;<strong>gain clairty</strong>&nbsp;and&nbsp;<strong>ask questions</strong>&nbsp;from time to time</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"lock":{"move":true,"remove":true},"className":"mt-auto","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-buttons mt-auto"><!-- wp:button {"backgroundColor":"secondary","style":{"border":{"radius":"10px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" href="https://docs.google.com/forms/d/e/1FAIpQLSf74ACtDJG9BNaxxfwZcXZDLP45ESWHjMOCIbSukA_eGC5qHQ/viewform" style="border-radius:10px" target="_blank" rel="noreferrer noopener">Apply</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|base","left":"var:preset|spacing|base"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--base);padding-left:var(--wp--preset--spacing--base)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p>Everything in the 1:1 coaching plan plus...</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p><strong>1x/month phone or Zoom call</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p>Anytime messaging for questions</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"className":"d-flex flex-column","style":{"spacing":{"blockGap":"var:preset|spacing|base"}}} -->
+<div class="wp-block-column d-flex flex-column"><!-- wp:group {"metadata":{"name":"Pricing Card"},"style":{"border":{"radius":"20px","width":"2px"},"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"},"blockGap":"var:preset|spacing|base"}},"borderColor":"drop-shadow","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-border-color has-drop-shadow-border-color" style="border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--lg);flex-direction:column"><!-- wp:group {"templateLock":"insert","lock":{"move":false,"remove":true},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group" style="flex-direction:row"><!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-light"}}},"typography":{"lineHeight":"1"}},"textColor":"primary-light","fontSize":"xxxl"} -->
+<p class="has-primary-light-color has-text-color has-link-color has-xxxl-font-size" style="line-height:1">$178</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-light"}}}},"textColor":"primary-light","fontSize":"md"} -->
+<p class="has-primary-light-color has-text-color has-link-color has-md-font-size">/mo</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"level":3,"lock":{"move":false,"remove":true}} -->
+<h3 class="wp-block-heading">Bronze</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This is for you if:&nbsp;You&nbsp;<strong>know how to track macros</strong>&nbsp;but you need&nbsp;<strong>accountability</strong>,&nbsp;<strong>support</strong>&nbsp;and&nbsp;<strong>adjustments</strong>&nbsp;along the way.<br><a href="https://docs.google.com/forms/d/e/1FAIpQLSf74ACtDJG9BNaxxfwZcXZDLP45ESWHjMOCIbSukA_eGC5qHQ/viewform" target="_blank" rel="noreferrer noopener"></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"lock":{"move":true,"remove":true},"className":"mt-auto","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-buttons mt-auto"><!-- wp:button {"style":{"border":{"radius":"10px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://docs.google.com/forms/d/e/1FAIpQLSf74ACtDJG9BNaxxfwZcXZDLP45ESWHjMOCIbSukA_eGC5qHQ/viewform" style="border-radius:10px" target="_blank" rel="noreferrer noopener">Apply</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"flex-shrink-0 flex-grow-1","style":{"spacing":{"padding":{"right":"var:preset|spacing|base","left":"var:preset|spacing|base"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group flex-shrink-0 flex-grow-1" style="padding-right:var(--wp--preset--spacing--base);padding-left:var(--wp--preset--spacing--base)"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-primary-color has-text-color has-link-color" style="flex-direction:row"><!-- wp:font-awesome/icon {"iconLayers":[{"iconDefinition":{"iconName":"check","prefix":"fas","icon":[448,512,null,null,"M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"]},"spin":false,"transform":null,"color":"rgb(140, 101, 72)","style":{"fontSize":"1.5em"}}]} -->
+<div class="wp-block-font-awesome-icon wp-font-awesome-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="font-size:1.5em" color="rgb(140, 101, 72)"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></div>
+<!-- /wp:font-awesome/icon -->
+
+<!-- wp:paragraph -->
+<p>Everything in the 1:1 coaching plan</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
