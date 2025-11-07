078f65f3c950fc5c6a3085ff4c720f62309fc3b6
KJ Roelke <kj.roelke@gmail.com>
2025-11-06 21:46:19 -0600

    update styles

--- patterns/footer.php
+++ patterns/footer.php
@@ -11,10 +11,9 @@
 
 ?>
 <!-- wp:group {"tagName":"footer","metadata":{"name":"Footer"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|secondary-light"}}}},"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"},"blockGap":"var:preset|spacing|xl","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"center"}} -->
-<footer class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);flex-direction:column">
-	<!-- wp:navigation {"ref":606,"overlayMenu":"never","icon":"menu","style":{"spacing":{"blockGap":"var:preset|spacing|base"}},"fontSize":"root","layout":{"type":"flex","justifyContent":"center"}} /-->
+<footer class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)"><!-- wp:navigation {"ref":606,"overlayMenu":"never","icon":"menu","style":{"spacing":{"blockGap":"var:preset|spacing|base"}},"fontSize":"root","layout":{"type":"flex","justifyContent":"center"}} /-->
 
-	<!-- wp:paragraph {"align":"center","fontSize":"root"} -->
-	<p class="has-text-align-center has-root-font-size" id="copyright">© Macros By Sara, LLC</p>
-	<!-- /wp:paragraph -->
-</footer><!-- /wp:group -->
+<!-- wp:paragraph {"align":"center","fontSize":"root"} -->
+<p class="has-text-align-center has-root-font-size" id="copyright">© Macros By Sara, LLC</p>
+<!-- /wp:paragraph --></footer>
+<!-- /wp:group -->
--- patterns/pricing-card.php
+++ patterns/pricing-card.php
@@ -9,9 +9,9 @@
  */
 
 ?>
-<!-- wp:group {"metadata":{"name":"Pricing Card"},"style":{"border":{"width":"2px","color":"rgb(90, 88, 92)","radius":"20px"},"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"},"blockGap":"var:preset|spacing|base"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group has-border-color" style="border-color:rgb(90, 88, 92);border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--lg);flex-direction:column"><!-- wp:group {"templateLock":"insert","lock":{"move":false,"remove":true},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
-<div class="wp-block-group" style="flex-direction:row"><!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-light"}}},"typography":{"lineHeight":"1"}},"textColor":"primary-light","fontSize":"xxxl"} -->
+<!-- wp:group {"metadata":{"name":"Pricing Card"},"style":{"border":{"width":"2px","color":"rgb(90, 88, 92)","radius":"20px"},"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-border-color" style="border-color:rgb(90, 88, 92);border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--lg)"><!-- wp:group {"lock":{"move":false,"remove":true},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"lock":{"move":true,"remove":true},"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-light"}}},"typography":{"lineHeight":"1"}},"textColor":"primary-light","fontSize":"xxxl"} -->
 <p class="has-primary-light-color has-text-color has-link-color has-xxxl-font-size" style="line-height:1">$249</p>
 <!-- /wp:paragraph -->
 
@@ -25,10 +25,10 @@
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>This is for you if you are: <strong><em>new</em> to tracking macros</strong> or <strong>want extra support with access to ask questions anytime</strong></p>
+<p>This is for you if you are:&nbsp;<strong><em>new</em>&nbsp;to tracking macros</strong>&nbsp;or&nbsp;<strong>want extra support with access to ask questions anytime</strong></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons {"lock":{"move":true,"remove":true}} -->
+<!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://docs.google.com/forms/d/e/1FAIpQLSf74ACtDJG9BNaxxfwZcXZDLP45ESWHjMOCIbSukA_eGC5qHQ/viewform" target="_blank" rel="noreferrer noopener">Apply</a></div>
 <!-- /wp:button --></div>
