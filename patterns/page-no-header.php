9ec1b1340f34596e1cd806c455bd450223cd0cea
KJ Roelke <kj.roelke@gmail.com>
2025-11-09 11:59:06 -0600

    add templates

---
+++ patterns/page-no-header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Page (Blank, No Header)
+ * Slug: mbs/page-no-header
+ * Categories: Layouts
+ * Keywords: Blank, No Header, Page
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A blank landing page with no header section.
+ *
+ * @package MacrosBySara
+ */
+
+?>
+<!-- wp:group {"tagName":"main","lock":{"move":true,"remove":true},"metadata":{"name":"Main"},"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group alignfull">
+	<!-- wp:post-content {"align":"full","layout":{"type":"constrained"}} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","theme":"macros-by-sara","align":"full"} /-->
