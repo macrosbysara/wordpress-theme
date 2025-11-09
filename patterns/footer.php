9354f0a2d43c6069c6220eb0c66c980b68068863
KJ Roelke <kj.roelke@gmail.com>
2025-11-09 13:16:58 -0600

    update footer template

--- patterns/footer.php
+++ patterns/footer.php
@@ -13,7 +13,7 @@
 <!-- wp:group {"tagName":"footer","metadata":{"name":"Footer"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|secondary-light"}}}},"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"},"blockGap":"var:preset|spacing|xl","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"center"}} -->
 <footer class="wp-block-group has-white-color has-primary-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)"><!-- wp:navigation {"ref":606,"overlayMenu":"never","icon":"menu","style":{"spacing":{"blockGap":"var:preset|spacing|base"}},"fontSize":"root","layout":{"type":"flex","justifyContent":"center"}} /-->
 
-<!-- wp:paragraph {"align":"center","fontSize":"root"} -->
-<p class="has-text-align-center has-root-font-size" id="copyright">© Macros By Sara, LLC</p>
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"root"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color has-root-font-size" id="copyright">© Macros By Sara, LLC</p>
 <!-- /wp:paragraph --></footer>
 <!-- /wp:group -->
